21ae5f04f1917fd4f74574cb70f4e4621dc6c1de
Guilherme Patricio <guigacp@hotmail.com>
2022-03-30 15:33:16 -0300

    GERACAO COMPONENT

---
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.css']
+})
+export class EventosComponent implements OnInit {
+
+  public eventos: any
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.geteventos();
+  }
+
+  public geteventos(): void {
+    this.http.get('https://localhost:5001/api/eventos').subscribe(
+      response => this.eventos = response,
+      error => console.log(error),
+    );
+  }
+}
